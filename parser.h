02accb0fbdeb6a678afdcf4af07e8e6e173effdb
jiaping_wang <wangjp9802@gmail.com>
2018-02-25 14:35:01 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -5,6 +5,6 @@
 char* readLine();
 int parseCommands(char* line, char** command);
 int parseSegments(char* command, char** segments);
-int parseArguments(char* segments, char* arguments);
+int parseArguments(char* segments, char** arguments);
 int check_last_character_in_process(char* process_line);
 
