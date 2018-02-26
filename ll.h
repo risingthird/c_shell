a354d87a04105cbb0dc7caf36747b218a7d6c939
Zhanpeng Wang <zwang10@haverford.edu>
2018-02-25 19:15:44 -0500

    Update ll.h

--- ll.h
+++ ll.h
@@ -1,47 +1,47 @@
 #include "util.h"
 
-typedef struct Process_Node {
-    char** args;
+typedef struct Process_Node
+{
+    char **args;
     int argn;
     pid_t pid;
     int status;
     //struct Process_Node* prev;
-    struct Process_Node* next;
+    struct Process_Node *next;
 } Process;
 
 typedef struct bgpr
 {
-	char* line;
-	int status;
-	int jobId;
-	pid_t pgid;	
-	Process* processList;
+    char *line;
+    int status;
+    int jobId;
+    pid_t pgid;
+    Process *processList;
 } Job;
 
-typedef struct LinkedList {
-    Job* job;
-    struct LinkedList* next;
-    struct LinkedList* prev;
+typedef struct LinkedList
+{
+    Job *job;
+    struct LinkedList *next;
+    struct LinkedList *prev;
 } Node;
 
-Node* head;
-Node* tail;
+Node *head;
+Node *tail;
 int size;
 int id;
 
-int jobRemovePid(pid_t pid);
-int jobRemoveJobId(int job_id);
-int jobInsert(Job* toAdd);
-int jobInit();
-Job* createJob(char* line, Process* process, int status);
-Job* getJobJobId(int job_id);
-Job* getJobPid(pid_t pid);
-void printList();
+int jobRemovePid(pid_t pid);    // remove job by pid
+int jobRemoveJobId(int job_id); // remove job by job id
+int jobInsert(Job *toAdd);      // add the job to the job list
+int jobInit();                  // initiate the job list.
+Job *createJob(char *line, Process *process, int status);
+Job *getJobJobId(int job_id); // get the current job by job id
+Job *getJobPid(pid_t pid);    // get the current job by pid
+void printList();             // print the list
 //void printJob();
-void freeArgs(char** args);
-void freeProcess(Process* process);
-void freeJob(Job* job);
-void freeJobList();
-void jobSetPGid(Job* job, pid_t pgid);
-void jobChangeStatus(Job* job, int status);
-
+void freeProcess(Process *process);
+void freeJob(Job *job);                     // free the job from memory
+void freeJobList();                         // free the job list
+void jobSetPGid(Job *job, pid_t pgid);      // set the process group id of the current job with a given pgid.
+void jobChangeStatus(Job *job, int status); // change the status of the current job given a status code
