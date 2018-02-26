1b24fe61664d401f386120d635a3e39e79e66d57
Zhanpeng Wang <zwang10@haverford.edu>
2018-02-25 19:18:42 -0500

    Print the shell prompt, sigaction in progress

--- shell.c
+++ shell.c
@@ -11,6 +11,7 @@ structure inspired by: https://github.com/jmreyes/simple-c-shell/blob/master/sim
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <termios.h>
+#include <pwd.h>
 #include "parser.h"
 #include "run_command.h"
 #include "ll.h"
@@ -18,8 +19,12 @@ structure inspired by: https://github.com/jmreyes/simple-c-shell/blob/master/sim
 #define MAXLEN 256
 #define MAXLINE 1024
 
-void myPrompt();
+// function prototypes.
+void ourPrompt();
 void initShell();
+void sigchld_handler(int sig, siginfo_t *sif, void *notused);
+
+// global variables
 char** argsWithPipe;  //  xxx | xxx | xxx | ... 
 char** args; // arguments in each *command
 char** command; // command line that has been separated by ";"
@@ -29,14 +34,94 @@ int myShTerminal;
 pid_t myShPGid;
 struct termios myShTmodes;
 
+void sigchld_handler(int sig, siginfo_t *sif, void *notused) {
+	int status;
+	// first get the process id of the child process and get the pgid of the child process
+	pid_t pgid = getpgid(sif->si_pid);
+	// Then get the job with pgid;
+	Job* job = getJobPid(pgid);
+	//check whether the calling process is the one that has changed status, wait without blocking
+	if (sif->si_pid == waitpid(pid, &status, WNOHANG | WUNTRACED)) {
+		//check what is the exit status of the calling process
+		if (WIFEXITED(status)) 
+		    {
+		    	if(jobRemovePid(pid) != 1) {
+						printf("Job removal from the job list error.\n");
+					}
+					
+		      printf ("Voluntary exit.\n");
+		      return;
+		    }
+		    if (WIFSTOPPED(status)) 
+		    {
+		      printf ("Suspended.\n");
+		      return;
+		    }
+		    if ( (WTERMSIG(status) <= 12) || (WTERMSIG(status) == 15))
+		    {
+		      printf ("Croaked");
+		      return;
+		    }
+		    printf ("Nothing interesting\n");
+			return;
+		}
+	else{
+		//if no update, print not interested.
+		printf("Not interested\n");
+	}
+}
+
+// print our shell prompt
+void ourPrompt() {
+  struct passwd *pwd; // using struct passwd from pwd.h
+  char host[MAXLEN];
+  char path[MAXLINE];
+  pwd = getpwuid(getuid());  // get information about the user.
+  getcwd(path, MAXLINE); // get the current working directory
+  char *username = pwd->pw_name; // username
+  char *homedirectory = pwd->pw_dir; // home directory
+  
+  if (gethostname(host, MAXLEN) == 0) {
+    printf("[用心写bug，用脚写shell, only bugs live forever ┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻, ¯\\_(ツ)_/¯ whatevs Broken Shell]\n[%s@%s ", username, host);
+  } else
+    printf("[用心写bug，用脚写shell, only bugs live forever ┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻, ¯\\_(ツ)_/¯ whatevs Broken Shell]\n[%s@unknown ", username);
+
+  if (strlen(path) < strlen(homedirectory) || strncmp(path, homedirectory, strlen(homedirectory)))
+    printf("%s]", path);
+  else
+    printf("~%s]",path);
+
+  // root: /  guest/user: $
+  if (geteuid() == 0)
+    printf("/ ");
+  else
+    printf("$ ");
+
+  return;
+}
 
 int main(int argc, char** argv) {
 	initShell();
 	jobInit();
+	
+	/* Sigchild signal handling*/
+	//declare the sample sigaction struct
+	struct sigaction sa;
+	//set the handler, in this case our handler has three arguments, so will set sa_sigaction instead of sa_handler.
+	sa.sa_sigaction = &sigchld_handler;
+	//initialize an empty set for signals to be blocked.
+	sigemptyset(&sa.sa_mask);
+	//set flags, SA_SIGINFO flag is to specify signal handler in sa is sa_sigaction.
+	sa.sa_flags = SA_SIGINFO;
+	//register the signal SIGCHLD.
+	sigaction (SIGCHLD, &sa, NULL);
+
 	int numCommands;
 	int numArguments;
 	int numSegments;
 	while(1) {
+		// print our shell prompt
+		ourPrompt();
 		command = (char**) malloc(MAXLINE * sizeof(char*));
 		myPrompt();
 		if((line = readline(">>>>>>>")) == NULL) {
@@ -78,4 +163,3 @@ int main(int argc, char** argv) {
 	return 0;
 
 }
-
