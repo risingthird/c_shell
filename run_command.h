84d54fc2875c4b8e80d4159c068d226edfd100d6
Zhanpeng Wang <zwang10@haverford.edu>
2018-02-27 14:44:42 -0500

    add sigset_t

--- run_command.h
+++ run_command.h
@@ -8,15 +8,15 @@
 #include "util.h"
 
 void bKill(char** args, int argn);
-void bFg(char** args, int argn);
+void bFg(char** args, int argn, sigset_t child_mask);
 void bBg(char** args, int argn);
 void bExit();
 void bJobs();
-void executing_command_without_pipe(Job* job);
-void jobs_lock();
-void jobs_unlock();
-int exeBuiltIn(char** args, int argn);
+void executing_command_without_pipe(Job* job, sigset_t child_mask);
+void jobs_lock(sigset_t child_mask);
+void jobs_unlock(sigset_t child_mask);
+int exeBuiltIn(char** args, int argn, sigset_t child_mask);
 int changeDirectory(char** args);
 int check_built_in(Job* job);
-void put_job_in_foreground(Job* job);
+void put_job_in_foreground(Job* job, sigset_t child_mask);
 #endif
